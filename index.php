cd61c8223bc293224c3daa1fc2e9fa4957162f60
Drekthar <nyancuisse@cuisse.net>
2018-11-25 04:42:03 +0100

    sidebar3

--- index.php
+++ index.php
@@ -10,11 +10,9 @@ require('Traitement/bdd/Connect.php')
     <link rel="stylesheet" href="Style/style.css" />
 </head>
 <body>
-<div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="mySidebar">
-    <img id="openNav" class="w3-button" onclick="w3_close()" width="50" height="50" src="Style/leftarrow.svg" />
-    <a href="#" class="w3-bar-item w3-button">Link 1</a>
-    <a href="#" class="w3-bar-item w3-button">Link 2</a>
-    <a href="#" class="w3-bar-item w3-button">Link 3</a>
+<div class="sidebar card animate-left" style="display:none" id="mySidebar">
+    <div class="button1" style="margin-top:10%;background-color: blueviolet;"><a href="#" >Administration du projet</a></div>
+    <img id="closeNav" onclick="w3_close()" width="50" height="50" src="Style/leftarrow.svg" />
 </div>
 
 <div id="main">
@@ -23,15 +21,15 @@ require('Traitement/bdd/Connect.php')
         <div class="w3-container">
             <h1 style="color:white;text-align:center">Endawn</h1>
         </div>
-        <img id="openNav" class="w3-button" onclick="w3_open()" width="50" height="50" src="Style/rightarrow.svg" />
+        <img id="openNav" onclick="w3_open()" width="50" height="50" src="Style/rightarrow.svg" />
 
     </div>
 
 </body>
 <script>
     function w3_open() {
-        document.getElementById("main").style.marginLeft = "25%";
-        document.getElementById("mySidebar").style.width = "25%";
+        document.getElementById("main").style.marginLeft = "15%";
+        document.getElementById("mySidebar").style.width = "15%";
         document.getElementById("mySidebar").style.display = "block";
         document.getElementById("openNav").style.display = 'none';
     }
