29a686fef94739497e2865f6ab21efa4bd47d1e0
Myns <mickaellebas59@outlook.fr>
2018-11-25 13:56:18 +0100

    0.6

--- inscriptionform.php
+++ inscriptionform.php
@@ -19,18 +19,20 @@ require('Traitement/bdd/Connect.php')
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="Style/normalize.css" />
     <link rel="stylesheet" href="Style/style.css" />
+    <link rel="stylesheet" href="Style/insform.css" />
 </head>
 <body>
-<div id="topbar"></div>
-<h1 class="maintitle">Endawn</h1>
+<div id="topbar">
+<a href="index.php"><h1 class="maintitle">Endawn</h1></a>
 <a href="connexion.php"><h3 id="connect" class="title">Connexion</h3></a>
 <a href="inscriptionform.php"><h3 id="inscription" class="title">Inscription</h3></a>
-
+</div>
 <div class="sidebar card animate-left" style="display:none" id="mySidebar">
     <div class="button1" id="adminproj"><a href="#"><h3 class="title">Administration du projet</h3></a></div>
     <div class="button1" id="News"><a href="#"><h3 class="title">News</h3></a></div>
     <div class="button1" id="Forum"><a href="#"><h3 class="title">Forum</h3></a></div>
     <div class="button1" id="Revue"><a href="#"><h3 class="title">Revue</h3></a></div>
+    <div class="button1" id="Timeline"><a href="#"><h3 class="title">Timeline</h3></a></div>
     <div class="button1" id="adminsite"><a href="#"><h3 class="title">Administration du site</h3></a></div>
 
     <img id="closeNav" onclick="w3_close()" width="50" height="50" src="Style/leftarrow.svg" />
@@ -41,10 +43,7 @@ require('Traitement/bdd/Connect.php')
     <img id="openNav" onclick="w3_open()" width="50" height="50" src="Style/rightarrow.svg" />
 
 
-
-
-
-    <form method="post" action="Traitement/Inscription.php" enctype="multipart/form-data" style="max-width:500px;margin:auto">
+    <form id="forminscription" method="post" action="Traitement/Inscription.php" enctype="multipart/form-data">
         <h2>Inscription</h2>
         <i class="fa fa-user icon"></i>
         <input class="input-field" type="text" id="nom" name="nom" placeholder="Nom" required oninvalid="this.setCustomValidity('Veuillez renseigner tous les champs !')">
