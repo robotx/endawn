02fe48af1fd60ee99a40ea028d76970f64e7a106
Drekthar <nyancuisse@cuisse.net>
2018-11-25 02:04:54 +0100

    nya first commit

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
-
-
-
-
+require('Traitement/Connect.php')
 ?>
 
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="Style/normalize.css" />
+    <link rel="stylesheet" href="Style/style.css" />
+</head>
+<body>
+
+<div id="topbar" />
+
+
+</body>
+</html>
 
