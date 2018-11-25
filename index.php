b0252001a3ded264901fa6d7a18e68ca4a7da75c
Drekthar <nyancuisse@cuisse.net>
2018-11-25 13:41:49 +0100

    font

--- index.php
+++ index.php
@@ -10,18 +10,19 @@ require('Traitement/bdd/Connect.php')
     <link rel="stylesheet" href="Style/style.css" />
 </head>
 <body>
-<div id="topbar"></div>
+<div id="topbar">
 <a href="index.php"><h1 class="maintitle">Endawn</h1></a>
 <a href="connexion.php"><h3 id="connect" class="title">Connexion</h3></a>
 <a href="inscriptionform.php"><h3 id="inscription" class="title">Inscription</h3></a>
+</div>
 
 <div class="sidebar card animate-left" style="display:none" id="mySidebar">
-    <div class="button1" id="adminproj"><a href="#"><h3 class="title">Administration du projet</h3></a></div>
-    <div class="button1" id="News"><a href="#"><h3 class="title">News</h3></a></div>
-    <div class="button1" id="Forum"><a href="#"><h3 class="title">Forum</h3></a></div>
-    <div class="button1" id="Revue"><a href="#"><h3 class="title">Revue</h3></a></div>
-    <div class="button1" id="Timeline"><a href="#"><h3 class="title">Timeline</h3></a></div>
-    <div class="button1" id="adminsite"><a href="#"><h3 class="title">Administration du site</h3></a></div>
+    <a href="#"><div class="button1" id="adminproj"><h3 class="title">Administration du projet</h3></div></a>
+    <a href="#"><div class="button1" id="News"><h3 class="title">News</h3></div></a>
+    <a href="#"><div class="button1" id="Forum"><h3 class="title">Forum</h3></div></a>
+    <a href="#"><div class="button1" id="Revue"><h3 class="title">Revue</h3></div></a>
+    <a href="#"><div class="button1" id="Timeline"><h3 class="title">Timeline</h3></div></a>
+    <a href="#"><div class="button1" id="adminsite"><h3 class="title">Administration du site</h3></div></a>
 
     <img id="closeNav" onclick="w3_close()" width="50" height="50" src="Style/leftarrow.svg" />
 </div>
