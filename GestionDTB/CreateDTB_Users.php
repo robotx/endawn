e4926649d256dcbef1bf5838a65ed8cb5caa0840
Drekthar <nyancuisse@cuisse.net>
2018-11-28 17:40:27 +0100

    dc button

--- GestionDTB/CreateDTB_Users.php
+++ GestionDTB/CreateDTB_Users.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "mimidu59200";
+$password = "root";
 
 try {
     $conn = new PDO("mysql:host=$servername", $username, $password);
