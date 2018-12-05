60fcb9035fc3af07a0dd27ca28ab11671202c653
myns18 <Mimidu25200>
2018-12-05 19:20:29 +0100

    0.26
    TEST 3

--- test2.php
+++ test2.php
@@ -1,16 +1,4 @@
 <?php
-ini_set('display_errors', 'on');
-
-?><font color="green"><big>Vous allez reçevoir un mail pour la validation de votre compte</big></font><br/><?php
-
-        $mail_destinataire = "jzolivier@gmail.com";
-        $sujet = "Validation de l'inscription sur le site Endawn";
-        $message = "TEST\n".
-        "Ceci est un test\n".
-
-        "Cordialement,\n".
-        "Administrateur";
-        $head = "Bonjour ";
-        mail($mail_destinataire, $sujet, $message, $head);
+phpinfo();
 
 ?>
