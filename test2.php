c1caa0ccc9a3fdccaff93e7dbf7e0f55e3bfa8ed
myns18 <Mimidu25200>
2018-12-05 13:07:53 +0100

    0.25
    TEST

--- test2.php
+++ test2.php
@@ -1,5 +1,14 @@
 <?php
-$mfr = $_GET["mfr"];
+require_once ('Traitement/bdd/Connect.php');
+require_once ('Outils/glofunction.php');
 
-echo $mfr;
+$sql = "SELECT * FROM Users";
+try {
+    $select = $pdo->prepare($sql);
+    $select->execute();
+} catch (PDOException $e){erreur("98","t_co_error_2");} //echo 'Erreur SQL : '. $e->getMessage().'<br/>'; die(); }
+while($data = $select->fetch())
+{
+    echo $data["pseudo"];
+}
 ?>
